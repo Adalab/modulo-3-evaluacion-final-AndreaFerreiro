5767f74d64cb744da6a8a8199af80e00140e3a59
“AndreaFerreiro” <andreafga28@gmail.com>
2023-06-30 16:11:39 +0200

    do npm install

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,25 @@
+import { useEffect, useState } from 'react';
 import '../styles/App.css';
 import {useLocation, matchPath} from 'react-router';
 import {Link, Route, Routes, useParams} from 'react-router-dom';
+import {useEffect, useState} from 'react';
 function App() {
   return (
     <div>
-      <h1>Hola Mundo</h1>
+      <hero>
+        <img src='../images/rickymorty.jpeg'></img>
+      </hero>
+      <main>
+        <form>
+          <label htmlFor='name'>
+            <input type='text' placeholder='Introduce el nombre del personaje que quieras buscar' id='name' htmlFor='name'></input>
+          </label>
+          <input type='submit' value={Buscar}></input>
+        </form>
+        <section>
+
+        </section>
+      </main>
     </div>
   );
 }
---
+++ src/services/Api.js
@@ -0,0 +1,9 @@
+const callToApi = (data)=>{
+    return fetch('https://dev.adalab.es/api/projectCard', {
+        method: 'POST',
+        body: JSON.stringify(data),
+        headers: { 'content-type': 'application/json' },
+      })
+    .then((response)=> response.json())
+  }
+  export default callToApi;

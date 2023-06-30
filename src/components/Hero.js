84d6082985eace961817073f09367b534b0d5f66
“AndreaFerreiro” <andreafga28@gmail.com>
2023-06-30 19:50:06 +0200

    add hero img

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,7 +1,7 @@
 const Hero = () => {
   return (
     <hero className="hero">
-      <img className="hero__img" src="../images/rickymorty.jpeg"></img>
+      <img className="hero__img" src="https://www.vodafone.es/c/statics/imagen/img_OG_Rick_y_Morty_T4_V2.jpg"></img>
     </hero>
   );
 };

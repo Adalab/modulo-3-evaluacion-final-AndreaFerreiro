2abfaef9db0c532f42a0ce57505c59ca31284a39
“AndreaFerreiro” <andreafga28@gmail.com>
2023-07-03 14:25:34 +0200

    fix error

--- src/components/Form.js
+++ src/components/Form.js
@@ -32,6 +32,7 @@ const Form = ({ handleSearch, nameSearch, filterCharacters, specieSearch }) => {
             placeholder="Specie"
             id="specie"
             htmlFor="specie"
+            value={specieSearch}
             onInput={handleSearch}
           ></input>
         </label>
